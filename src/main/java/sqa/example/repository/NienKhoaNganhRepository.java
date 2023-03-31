87e419fa375b2dc074c764ab151c9703b4c23edd
huysvp <nvh.viethuy@gmail.com>
2023-03-31 23:11:08 +0700

    Update NienKhoaNganhRepository.java

--- src/main/java/sqa/example/repository/NienKhoaNganhRepository.java
+++ src/main/java/sqa/example/repository/NienKhoaNganhRepository.java
@@ -7,14 +7,16 @@ import org.springframework.stereotype.Repository;
 import sqa.example.model.NienKhoaNganh;
 
 @Repository
-public interface NienKhoaNganhRepository extends JpaRepository<NienKhoaNganh, Integer> {
+public interface NienKhoaNganhRepository extends JpaRepository<NienKhoaNganh, Integer> 
+{
     @Query(value = "SELECT * FROM tbl_nien_khoa_nganh nkn WHERE nkn.id = :user_id LIMIT 1", nativeQuery = true)
     NienKhoaNganh getNienKhoaNganhOfSinhVien(@Param("user_id") Integer user_id);
-    @Query(value = "SELECT * FROM tbl_nien_khoa_nganh nkn WHERE nkn.id = :nien_khoa_nganh_nam_hoc_ky_hoc_id LIMIT 1", nativeQuery = true)
+    
+	@Query(value = "SELECT * FROM tbl_nien_khoa_nganh nkn WHERE nkn.id = :nien_khoa_nganh_nam_hoc_ky_hoc_id LIMIT 1", nativeQuery = true)
     NienKhoaNganh getNienKhoaNganhByNienKhoaNganhNamHocKyHocId(@Param("nien_khoa_nganh_nam_hoc_ky_hoc_id") Integer id);
-    @Query(value = "SELECT * FROM tbl_nien_khoa_nganh nkn WHERE nkn.nien_khoa_id = " +
-            ":nien_khoa_id AND nkn.nganh_id = :nganh_id", nativeQuery = true)
-    NienKhoaNganh getNienKhoaNganhByNienKhoaAndNganh(@Param("nien_khoa_id") Integer nien_khoa_id,
-                                                     @Param("nganh_id") Integer nganh_id) ;
+    
+	@Query(value = "SELECT * FROM tbl_nien_khoa_nganh nkn WHERE nkn.nien_khoa_id =:nien_khoa_id AND nkn.nganh_id = :nganh_id", nativeQuery = true)
+    NienKhoaNganh getNienKhoaNganhByNienKhoaAndNganh(@Param("nien_khoa_id") Integer nien_khoa_id, @Param("nganh_id") Integer nganh_id);
+	
     NienKhoaNganh getNienKhoaNganhByNienKhoaIdAndNganhId(Integer nienKhoaId, Integer nganhId);
 }
