c6c525fc7815814c124d3ad2a99a6cc0f60fce50
huysvp <nvh.viethuy@gmail.com>
2023-03-31 23:10:19 +0700

    Update NienKhoaNganhNamHocKyHocMonHocRepository.java

--- src/main/java/sqa/example/repository/NienKhoaNganhNamHocKyHocMonHocRepository.java
+++ src/main/java/sqa/example/repository/NienKhoaNganhNamHocKyHocMonHocRepository.java
@@ -11,18 +11,18 @@ import java.util.List;
 @Repository
 public interface NienKhoaNganhNamHocKyHocMonHocRepository extends JpaRepository<NienKhoaNganhNamHocKyHocMonHoc, Integer> {
     @Query(value = "SELECT * FROM tbl_nien_khoa_nganh_nam_hoc_ky_hoc_mon_hoc nknnhkhmh " +
-            "WHERE nknnhkhmh.nien_khoa_nganh_nam_hoc_ky_hoc_id = :nien_khoa_nganh_nam_hoc_ky_hoc_id", nativeQuery = true)
+				   "WHERE nknnhkhmh.nien_khoa_nganh_nam_hoc_ky_hoc_id = :nien_khoa_nganh_nam_hoc_ky_hoc_id", nativeQuery = true)
     List<NienKhoaNganhNamHocKyHocMonHoc> getNienKhoaNganhNamHocKyHocMonHoc
             (@Param("nien_khoa_nganh_nam_hoc_ky_hoc_id") Integer nien_khoa_nganh_nam_hoc_ky_hoc_id);
 
     @Query(value = "SELECT * FROM tbl_nien_khoa_nganh_nam_hoc_ky_hoc_mon_hoc nknnhkhmh " +
-            "WHERE nknnhkhmh.nien_khoa_nganh_nam_hoc_ky_hoc_id = :nien_khoa_nganh_nam_hoc_ky_hoc_id " +
-            "AND nknnhkhmh.mon_hoc_id = :mon_hoc_id", nativeQuery = true)
+				   "WHERE nknnhkhmh.nien_khoa_nganh_nam_hoc_ky_hoc_id = :nien_khoa_nganh_nam_hoc_ky_hoc_id " +
+				   "AND nknnhkhmh.mon_hoc_id = :mon_hoc_id", nativeQuery = true)
     NienKhoaNganhNamHocKyHocMonHoc getNienKhoaNganhNamHocKyHocMonHoc
             (@Param("nien_khoa_nganh_nam_hoc_ky_hoc_id") Integer nien_khoa_nganh_nam_hoc_ky_hoc_id,
              @Param("mon_hoc_id") Integer mon_hoc_id);
 
-    @Query(value = "SELECT * FROM tbl_nien_khoa_nganh_nam_hoc_ky_hoc_mon_hoc nknnhkhmh " +
+	@Query(value = "SELECT * FROM tbl_nien_khoa_nganh_nam_hoc_ky_hoc_mon_hoc nknnhkhmh " +
 				   "WHERE nknnhkhmh.mon_hoc_id = :mon_hoc_id", nativeQuery = true)
 	List<NienKhoaNganhNamHocKyHocMonHoc> getNienKhoaNganhNamHocKyHocMonHocByMonHoc
 			(@Param("mon_hoc_id") Integer mon_hoc_id);
