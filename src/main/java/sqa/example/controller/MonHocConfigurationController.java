cc854dbb47c8484d25b24a3db6d46508e6585e2e
huysvp <nvh.viethuy@gmail.com>
2023-04-03 22:48:01 +0700

    Update MonHocConfigurationController.java

--- src/main/java/sqa/example/controller/MonHocConfigurationController.java
+++ src/main/java/sqa/example/controller/MonHocConfigurationController.java
@@ -14,75 +14,48 @@ import java.util.*;
 public class MonHocConfigurationController {
     private final MonHocRepository monHocRepository;
 
-	// http://localhost:8080/api/v1/cau-hinh/mon-hocs
-    @GetMapping("mon-hocs")
-    public ResponseEntity<List<MonHoc>> getAll() 
+    @GetMapping("/mon-hocs")
+    public ResponseEntity<List<MonHoc>> all() 
 	{
         return ResponseEntity.ok(monHocRepository.findAll());
     }
 	
-	// http://localhost:8080/api/v1/cau-hinh/mon-hocs/nhap mon cong nghe phan mem
-	@GetMapping("mon-hocs/{mon-hoc-name}")
-    public ResponseEntity<MonHoc> getByName(@PathVariable(value = "mon-hoc-name") String mon_hoc_name) 
+	@GetMapping("/mon-hocs/{id}")
+    public ResponseEntity<MonHoc> one(@PathVariable("id") Integer id) 
 	{
-        return ResponseEntity.ok(monHocRepository.getMonHocByName(mon_hoc_name));
+        return ResponseEntity.ok(monHocRepository.findById(id).get());
     }
 
-	// http://localhost:8080/api/v1/cau-hinh/mon-hocs/mon hoc moi da den/them-moi?cc=0.2&th=0.2&kt=0.2&bt=0.2&ck=0.2
-	// Cac thong so diem thanh phan phai hop le, neu khong mon hoc se khong duoc them va tra ve ket qua rong.
-	@PostMapping("mon-hocs/{mon-hoc-name}/them-moi")
-	public ResponseEntity<MonHoc> create(@PathVariable(value = "mon-hoc-name") String mon_hoc_name,
-										 @RequestParam(value = "cc", required = true) Double cc,
-									     @RequestParam(value = "th", required = true) Double th,
-										 @RequestParam(value = "kt", required = true) Double kt,
-										 @RequestParam(value = "bt", required = true) Double bt,
-										 @RequestParam(value = "ck", required = true) Double ck) 
+	@PostMapping("/mon-hocs")
+	public ResponseEntity<MonHoc> create(@RequestBody MonHoc monHoc) 
 	{
-		if (monHocRepository.getMonHocByName(mon_hoc_name) != null)
+		if (monHocRepository.getMonHocByName(monHoc.getTen()) != null)
 			return ResponseEntity.ok(null);
 		
-		MonHoc monHoc = new MonHoc();
-		if (isInvalid(cc, th, kt, bt, ck))
+		if (isInvalid(monHoc))
 			return ResponseEntity.ok(null);
-		
-		monHoc.setTen(mon_hoc_name);
-		monHoc.setTyLeDiemCC(cc);
-		monHoc.setTyLeDiemTH(th);
-		monHoc.setTyLeDiemKT(kt);
-		monHoc.setTyLeDiemBT(bt);
-		monHoc.setTyLeDiemCuoiKy(ck);
-		
+	
 		return ResponseEntity.ok(monHocRepository.save(monHoc));
 	}
 	
-	// http://localhost:8080/api/v1/cau-hinh/mon-hocs/nhap mon cong nghe phan mem/chinh-sua?cc=0.2&th=0.2&kt=0.2&bt=0.2&ck=0.2
-	// Cac thong so diem thanh phan phai hop le, neu khong mon hoc se khong duoc sua va tra ve ket qua rong.
-	@PutMapping("mon-hocs/{mon-hoc-name}/chinh-sua")
-    public ResponseEntity<MonHoc> updateByName(@PathVariable(value = "mon-hoc-name") String mon_hoc_name,
-											   @RequestParam(value = "cc", required = true) Double cc,
-											   @RequestParam(value = "th", required = true) Double th,
-											   @RequestParam(value = "kt", required = true) Double kt,
-											   @RequestParam(value = "bt", required = true) Double bt,
-											   @RequestParam(value = "ck", required = true) Double ck) 
+	@PutMapping("/mon-hocs")
+    public ResponseEntity<MonHoc> update(@RequestBody MonHoc monHoc) 
 	{
-        var monHoc = monHocRepository.getMonHocByName(mon_hoc_name);	
-		if (monHoc == null || isInvalid(cc, th, kt, bt, ck))
+		if (isInvalid(monHoc))
 			return ResponseEntity.ok(null);
 		
-		monHoc.setTyLeDiemCC(cc);
-		monHoc.setTyLeDiemTH(th);
-		monHoc.setTyLeDiemKT(kt);
-		monHoc.setTyLeDiemBT(bt);
-		monHoc.setTyLeDiemCuoiKy(ck);
-		monHocRepository.save(monHoc);
-		return ResponseEntity.ok(monHoc);
+		return monHocRepository.findById(monHoc.getId()).map(mh -> {
+			return ResponseEntity.ok(monHocRepository.save(monHoc));
+		})
+		.orElseGet(() -> {
+			return ResponseEntity.ok(null);
+		});
     }
 	
-	// http://localhost:8080/api/v1/cau-hinh/mon-hocs/mon hoc moi da den/xoa
-	@DeleteMapping("mon-hocs/{mon-hoc-name}/xoa")
-	public ResponseEntity<Boolean> delete(@PathVariable(value = "mon-hoc-name") String mon_hoc_name) 
+	@DeleteMapping("/mon-hocs/{id}")
+	public ResponseEntity<Boolean> delete(@PathVariable("id") Integer id) 
 	{
-		var monHoc = monHocRepository.getMonHocByName(mon_hoc_name);
+		var monHoc = monHocRepository.findById(id).get();
 		if (monHoc == null)
 			return ResponseEntity.ok(false);
 		
@@ -90,12 +63,14 @@ public class MonHocConfigurationController {
 		return ResponseEntity.ok(true);
 	}
 	
-	private boolean isInvalid(Double cc, Double th, Double kt, Double bt, Double ck)
+	private boolean isInvalid(MonHoc monHoc)
 	{
 		boolean isInvalid = false;
-		isInvalid |= cc < 0 || th < 0 || kt < 0 || bt < 0;
-		isInvalid |= cc > 1 || th > 1 || kt > 1 || bt > 1;
-		isInvalid |= cc + th + kt + bt + ck != 1;
+		isInvalid |= monHoc.getTyLeDiemCC() < 0 || monHoc.getTyLeDiemTH() < 0 
+				  || monHoc.getTyLeDiemKT() < 0 || monHoc.getTyLeDiemBT() < 0 || monHoc.getTyLeDiemCuoiKy() < 0;
+		isInvalid |= monHoc.getTyLeDiemCC() > 1 || monHoc.getTyLeDiemTH() > 1 
+				  || monHoc.getTyLeDiemKT() > 1 || monHoc.getTyLeDiemBT() > 1 || monHoc.getTyLeDiemCuoiKy() > 1;
+		isInvalid |= monHoc.getTyLeDiemCC() + monHoc.getTyLeDiemTH() + monHoc.getTyLeDiemKT() + monHoc.getTyLeDiemBT() + monHoc.getTyLeDiemCuoiKy() != 1;
 		return isInvalid;
 	}
 }
