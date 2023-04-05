f96a43dfcf53db8a23f8a75c4e872e8b06fd293d
huysvp <nvh.viethuy@gmail.com>
2023-04-05 13:57:13 +0700

    Update NamHocService.java

--- src/main/java/sqa/example/service/NamHocService.java
+++ src/main/java/sqa/example/service/NamHocService.java
@@ -1,14 +1,21 @@
 package sqa.example.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sqa.example.model.NamHoc;
 import sqa.example.repository.NamHocRepository;
 
 @Service
 @RequiredArgsConstructor
 public class NamHocService {
     private final NamHocRepository namHocRepository;
+	
     public Integer getIdNamHocByName(String name) {
         return namHocRepository.getIdNamHocByName(name).getId();
     }
+	
+	public List<NamHoc> getAll() {
+		return namHocRepository.findAll();
+	}
 }
