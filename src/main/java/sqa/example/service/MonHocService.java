d5926e17775e3f088d27b0a6ad2121556a622f7e
huysvp <nvh.viethuy@gmail.com>
2023-04-05 13:56:20 +0700

    Update MonHocService.java

--- src/main/java/sqa/example/service/MonHocService.java
+++ src/main/java/sqa/example/service/MonHocService.java
@@ -1,14 +1,70 @@
 package sqa.example.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sqa.example.model.MonHoc;
 import sqa.example.repository.MonHocRepository;
 
 @Service
 @RequiredArgsConstructor
 public class MonHocService {
-    private final MonHocRepository monHocRepository;
-    public Integer getIdMonHocByName(String name) {
-        return monHocRepository.getMonHocByName(name).getId();
+    private final MonHocRepository entityRepository;
+	
+    public Integer getIdMonHocByName(String name) 
+	{
+        return entityRepository.getMonHocByName(name).getId();
     }
+	
+	public List<MonHoc> getAll() 
+	{
+		return entityRepository.findAll();
+	}
+	
+	public MonHoc get(Integer id) 
+	{
+        return entityRepository.findById(id).get();
+    }
+	
+	public MonHoc create(MonHoc entity) 
+	{
+		if (isInvalid(entity))
+			return null;
+		
+		if (entityRepository.getMonHocByName(entity.getTen()) != null)
+			return null;
+			
+		return entityRepository.save(entity);
+	}
+	
+	public MonHoc update(MonHoc entity) 
+	{
+		if (isInvalid(entity))
+			return null;
+		
+		if (entityRepository.findById(entity.getId()) != null)
+			return entityRepository.save(entity);
+		return null;
+    }
+	
+	public boolean delete(Integer id) 
+	{
+		var entity = entityRepository.findById(id).get();
+		if (entity == null)
+			return false;
+		
+		entityRepository.delete(entity);
+		return true;
+	}
+	
+	private boolean isInvalid(MonHoc entity)
+	{
+		boolean isInvalid = false;
+		isInvalid |= entity.getTyLeDiemCC() < 0 || entity.getTyLeDiemTH() < 0 
+				  || entity.getTyLeDiemKT() < 0 || entity.getTyLeDiemBT() < 0 || entity.getTyLeDiemCuoiKy() < 0;
+		isInvalid |= entity.getTyLeDiemCC() > 1 || entity.getTyLeDiemTH() > 1 
+				  || entity.getTyLeDiemKT() > 1 || entity.getTyLeDiemBT() > 1 || entity.getTyLeDiemCuoiKy() > 1;
+		isInvalid |= entity.getTyLeDiemCC() + entity.getTyLeDiemTH() + entity.getTyLeDiemKT() + entity.getTyLeDiemBT() + entity.getTyLeDiemCuoiKy() != 1;
+		return isInvalid;
+	}
 }
