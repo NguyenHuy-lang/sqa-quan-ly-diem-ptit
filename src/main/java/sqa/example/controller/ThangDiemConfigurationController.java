50f08882c0edd44d30c0320f4976c4c34afac83d
huysvp <nvh.viethuy@gmail.com>
2023-04-05 13:55:32 +0700

    Update ThangDiemConfigurationController.java

--- src/main/java/sqa/example/controller/ThangDiemConfigurationController.java
+++ src/main/java/sqa/example/controller/ThangDiemConfigurationController.java
@@ -4,56 +4,43 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sqa.example.model.*;
-import sqa.example.repository.*;
 
 import java.util.*;
+import sqa.example.service.ThangDiemService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/cau-hinh/")
 public class ThangDiemConfigurationController {
-    private final ThangDiemRepository thangDiemRepository;
+    private final ThangDiemService thangDiemService;
 
     @GetMapping("/thang-diems")
     public ResponseEntity<List<ThangDiem>> all() 
 	{
-        return ResponseEntity.ok(thangDiemRepository.findAll());
+        return ResponseEntity.ok(thangDiemService.getAll());
     }
 	
 	@GetMapping("/thang-diems/{id}")
     public ResponseEntity<ThangDiem> one(@PathVariable("id") Integer id) 
 	{
-        return ResponseEntity.ok(thangDiemRepository.findById(id).get());
+        return ResponseEntity.ok(thangDiemService.get(id));
     }
 
 	@PostMapping("/thang-diems")
 	public ResponseEntity<ThangDiem> create(@RequestBody ThangDiem thangDiem) 
 	{
-		if (thangDiemRepository.getThangDiemByName(thangDiem.getDiemChu()) != null)
-			return ResponseEntity.ok(null);
-		
-		return ResponseEntity.ok(thangDiemRepository.save(thangDiem));
+		return ResponseEntity.ok(thangDiemService.create(thangDiem));
 	}
 	
 	@PutMapping("/thang-diems")
     public ResponseEntity<ThangDiem> update(@RequestBody ThangDiem thangDiem) 
 	{
-		return thangDiemRepository.findById(thangDiem.getId()).map(mh -> {
-			return ResponseEntity.ok(thangDiemRepository.save(thangDiem));
-		})
-		.orElseGet(() -> {
-			return ResponseEntity.ok(null);
-		});
+		return ResponseEntity.ok(thangDiemService.update(thangDiem));
     }
 	
 	@DeleteMapping("/thang-diems/{id}")
 	public ResponseEntity<Boolean> delete(@PathVariable("id") Integer id) 
 	{
-		var thangDiem = thangDiemRepository.findById(id).get();
-		if (thangDiem == null)
-			return ResponseEntity.ok(false);
-		
-		thangDiemRepository.delete(thangDiem);
-		return ResponseEntity.ok(true);
+		return ResponseEntity.ok(thangDiemService.delete(id));
 	}
 }
