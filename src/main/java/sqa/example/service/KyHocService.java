96a6f78be9bf121fb80c66cf9d1bfe07adf9d0d2
huysvp <nvh.viethuy@gmail.com>
2023-04-05 13:56:41 +0700

    Update KyHocService.java

--- src/main/java/sqa/example/service/KyHocService.java
+++ src/main/java/sqa/example/service/KyHocService.java
@@ -1,5 +1,6 @@
 package sqa.example.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sqa.example.model.KyHoc;
@@ -7,10 +8,22 @@ import sqa.example.repository.KyHocRepository;
 
 @Service
 @RequiredArgsConstructor
-public class KyHocService {
+public class KyHocService 
+{
     private final KyHocRepository kyHocRepository;
-    public Integer getIdKyHocByNameKyHoc(String name) {
+    
+	public Integer getIdKyHocByNameKyHoc(String name) {
         KyHoc kyHoc = kyHocRepository.getKyHocByName(name);
         return kyHoc.getId();
     }
+	
+	public List<KyHoc> getAll() 
+	{
+		return kyHocRepository.findAll();
+	}
+	
+	public KyHoc getByName(String name) 
+	{
+		return kyHocRepository.getKyHocByName(name);
+	}
 }
