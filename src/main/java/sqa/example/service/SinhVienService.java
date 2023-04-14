f1bb446c83e4f1a88779b28cd5b02efbe1d08b1f
huysvp <nvh.viethuy@gmail.com>
2023-04-14 21:28:53 +0700

    Update SinhVienService.java

--- src/main/java/sqa/example/service/SinhVienService.java
+++ src/main/java/sqa/example/service/SinhVienService.java
@@ -1,4 +1,17 @@
 package sqa.example.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sqa.example.model.SinhVien;
+import sqa.example.repository.SinhVienRepository;
+
+@Service
+@RequiredArgsConstructor
 public class SinhVienService {
+	private final SinhVienRepository sinhVienRepository;
+
+	public SinhVien get(Integer id) 
+	{
+        return sinhVienRepository.findById(id).get();
+  }
 }
