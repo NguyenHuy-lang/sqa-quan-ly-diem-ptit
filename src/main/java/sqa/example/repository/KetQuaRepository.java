af49e64f45c87add546e704e78a429bc75500c6b
huysvp <nvh.viethuy@gmail.com>
2023-04-19 18:27:04 +0700

    Update KetQuaRepository.java

--- src/main/java/sqa/example/repository/KetQuaRepository.java
+++ src/main/java/sqa/example/repository/KetQuaRepository.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Repository;
 import sqa.example.model.KetQua;
 @Repository
 public interface KetQuaRepository extends JpaRepository<KetQua, Integer> {
-    @Query(value = "SELECT * FROM tbl_ket_qua kq WHERE kq.sinh_vien_id = :sinh_vien_id AND " +
-            "kq.lop_hoc_phan_id = :lop_hoc_phan_id", nativeQuery = true)
-    KetQua getKetQuaOfSinhVien(@Param("sinh_vien_id") Integer sinh_vien_id, @Param("lop_hoc_phan_id") Integer lop_hoc_phan_id);
-
+    @Query(value = "SELECT * FROM tbl_ket_qua kq WHERE kq.sinh_vien_id = :sinh_vien_id AND kq.lop_hoc_phan_id = :lop_hoc_phan_id", nativeQuery = true)
+    KetQua findBySinhVienAndLopHocPhanId(@Param("sinh_vien_id") Integer sinh_vien_id, @Param("lop_hoc_phan_id") Integer lop_hoc_phan_id);
 }
