ec0eae67ebdc85c848685cd2bd456e7663f0d81b
huysvp <nvh.viethuy@gmail.com>
2023-04-19 18:29:55 +0700

    Update NienKhoaNganhNamHocKyHocMonHocRepository.java

--- src/main/java/sqa/example/repository/NienKhoaNganhNamHocKyHocMonHocRepository.java
+++ src/main/java/sqa/example/repository/NienKhoaNganhNamHocKyHocMonHocRepository.java
@@ -10,20 +10,12 @@ import java.util.List;
 
 @Repository
 public interface NienKhoaNganhNamHocKyHocMonHocRepository extends JpaRepository<NienKhoaNganhNamHocKyHocMonHoc, Integer> {
-    @Query(value = "SELECT * FROM tbl_nien_khoa_nganh_nam_hoc_ky_hoc_mon_hoc nknnhkhmh " +
-				   "WHERE nknnhkhmh.nien_khoa_nganh_nam_hoc_ky_hoc_id = :nien_khoa_nganh_nam_hoc_ky_hoc_id", nativeQuery = true)
-    List<NienKhoaNganhNamHocKyHocMonHoc> getNienKhoaNganhNamHocKyHocMonHoc
-            (@Param("nien_khoa_nganh_nam_hoc_ky_hoc_id") Integer nien_khoa_nganh_nam_hoc_ky_hoc_id);
+    @Query(value = "SELECT * FROM tbl_nien_khoa_nganh_nam_hoc_ky_hoc_mon_hoc nknnhkhmh WHERE nknnhkhmh.nien_khoa_nganh_nam_hoc_ky_hoc_id = :nien_khoa_nganh_nam_hoc_ky_hoc_id AND nknnhkhmh.mon_hoc_id = :mon_hoc_id", nativeQuery = true)
+    NienKhoaNganhNamHocKyHocMonHoc findByNienKhoaNganhNamHocKyHocAndMonHocId(@Param("nien_khoa_nganh_nam_hoc_ky_hoc_id") Integer nien_khoa_nganh_nam_hoc_ky_hoc_id, @Param("mon_hoc_id") Integer mon_hoc_id);
 
-    @Query(value = "SELECT * FROM tbl_nien_khoa_nganh_nam_hoc_ky_hoc_mon_hoc nknnhkhmh " +
-				   "WHERE nknnhkhmh.nien_khoa_nganh_nam_hoc_ky_hoc_id = :nien_khoa_nganh_nam_hoc_ky_hoc_id " +
-				   "AND nknnhkhmh.mon_hoc_id = :mon_hoc_id", nativeQuery = true)
-    NienKhoaNganhNamHocKyHocMonHoc getNienKhoaNganhNamHocKyHocMonHoc
-            (@Param("nien_khoa_nganh_nam_hoc_ky_hoc_id") Integer nien_khoa_nganh_nam_hoc_ky_hoc_id,
-             @Param("mon_hoc_id") Integer mon_hoc_id);
-
-	@Query(value = "SELECT * FROM tbl_nien_khoa_nganh_nam_hoc_ky_hoc_mon_hoc nknnhkhmh " +
-				   "WHERE nknnhkhmh.mon_hoc_id = :mon_hoc_id", nativeQuery = true)
-	List<NienKhoaNganhNamHocKyHocMonHoc> getNienKhoaNganhNamHocKyHocMonHocByMonHoc
-			(@Param("mon_hoc_id") Integer mon_hoc_id);
+	@Query(value = "SELECT * FROM tbl_nien_khoa_nganh_nam_hoc_ky_hoc_mon_hoc nknnhkhmh WHERE nknnhkhmh.mon_hoc_id = :mon_hoc_id", nativeQuery = true)
+	List<NienKhoaNganhNamHocKyHocMonHoc> findAllByMonHocId(@Param("mon_hoc_id") Integer mon_hoc_id);
+	
+	@Query(value = "SELECT * FROM tbl_nien_khoa_nganh_nam_hoc_ky_hoc_mon_hoc nknnhkhmh WHERE nknnhkhmh.nien_khoa_nganh_nam_hoc_ky_hoc_id = :nien_khoa_nganh_nam_hoc_ky_hoc_id", nativeQuery = true)
+    List<NienKhoaNganhNamHocKyHocMonHoc> findAllByNienKhoaNganhNamHocKyHocId(@Param("nien_khoa_nganh_nam_hoc_ky_hoc_id") Integer nien_khoa_nganh_nam_hoc_ky_hoc_id);
 }
