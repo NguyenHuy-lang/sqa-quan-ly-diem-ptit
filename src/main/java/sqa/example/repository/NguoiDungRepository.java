3cde6733f679f8cfbd17f6a61257288c61b81d0f
huysvp <nvh.viethuy@gmail.com>
2023-04-19 18:29:27 +0700

    Update NguoiDungRepository.java

--- src/main/java/sqa/example/repository/NguoiDungRepository.java
+++ src/main/java/sqa/example/repository/NguoiDungRepository.java
@@ -3,6 +3,6 @@ package sqa.example.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sqa.example.model.NguoiDung;
+
 @Repository
-public interface NguoiDungRepository extends JpaRepository<NguoiDung, Integer> {
-}
+public interface NguoiDungRepository extends JpaRepository<NguoiDung, Integer> {}
