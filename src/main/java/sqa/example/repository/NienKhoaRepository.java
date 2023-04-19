ba85f3dee2a0071ad2bf1f7b1da2c5162d7b05a7
huysvp <nvh.viethuy@gmail.com>
2023-04-19 18:30:50 +0700

    Update NienKhoaRepository.java

--- src/main/java/sqa/example/repository/NienKhoaRepository.java
+++ src/main/java/sqa/example/repository/NienKhoaRepository.java
@@ -1,13 +1,8 @@
 package sqa.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import sqa.example.model.NienKhoa;
 
 @Repository
-public interface NienKhoaRepository extends JpaRepository<NienKhoa, Integer> {
-    @Query(value = "SELECT * FROM tbl_nien_khoa nk WHERE nk.ten = :ten", nativeQuery = true)
-    NienKhoa getNienKhoaByName(@Param("ten") String ten);
-}
+public interface NienKhoaRepository extends JpaRepository<NienKhoa, Integer> {}
