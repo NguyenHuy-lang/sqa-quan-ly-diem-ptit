29ff3cfd55d34f9bc5f765722d17f770f5182575
huysvp <nvh.viethuy@gmail.com>
2023-04-19 18:28:44 +0700

    Update NamHocRepository.java

--- src/main/java/sqa/example/repository/NamHocRepository.java
+++ src/main/java/sqa/example/repository/NamHocRepository.java
@@ -1,12 +1,8 @@
 package sqa.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import sqa.example.model.NamHoc;
+
 @Repository
-public interface NamHocRepository extends JpaRepository<NamHoc, Integer> {
-    @Query(value = "SELECT * FROM tbl_nam_hoc nh where nh.ten = :ten LIMIT 1", nativeQuery = true)
-    NamHoc getIdNamHocByName(@Param("ten") String name);
-}
+public interface NamHocRepository extends JpaRepository<NamHoc, Integer> {}
