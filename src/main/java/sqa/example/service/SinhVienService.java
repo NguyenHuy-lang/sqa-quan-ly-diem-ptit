3adb7f4e9a3dfe6739b43846aebe6bd4e50fe69f
huysvp <nvh.viethuy@gmail.com>
2023-04-19 18:35:46 +0700

    Update SinhVienService.java

--- src/main/java/sqa/example/service/SinhVienService.java
+++ src/main/java/sqa/example/service/SinhVienService.java
@@ -1,16 +1,43 @@
 package sqa.example.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import sqa.example.model.SinhVien;
-import sqa.example.repository.SinhVienRepository;
+import sqa.example.model.ThangDiem;
+import sqa.example.repository.ThangDiemRepository;
 
 @Service
 @RequiredArgsConstructor
-public class SinhVienService {
-	private final SinhVienRepository sinhVienRepository;
-
-	public SinhVien findById(Integer id) {
-        return sinhVienRepository.findById(id).get();
+public class ThangDiemService 
+{
+	private final ThangDiemRepository thangDiemRepository;
+	
+	public List<ThangDiem> findAll() {
+		return thangDiemRepository.findAll();
+	}
+	
+	public ThangDiem findById(Integer id) {
+        return thangDiemRepository.findById(id).get();
     }
+	
+	public ThangDiem findByName(String name) {
+		return thangDiemRepository.findByName(name);
+	}
+	
+	public ThangDiem create(ThangDiem entity) {		
+		return thangDiemRepository.save(entity);
+	}
+	
+	public ThangDiem update(ThangDiem thangDiem) {
+		return thangDiemRepository.findById(thangDiem.getId()).map(mh -> thangDiemRepository.save(thangDiem)).orElseGet(() -> null);
+    }
+	
+	public boolean delete(Integer id) {
+		var entity = thangDiemRepository.findById(id).get();
+		if (entity == null)
+			return false;
+		
+		thangDiemRepository.delete(entity);
+		return true;
+	}
 }
