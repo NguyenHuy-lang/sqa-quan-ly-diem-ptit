c44fd7f256e35d721c1f44940eab2676dc6240bf
huysvp <nvh.viethuy@gmail.com>
2023-04-19 18:33:33 +0700

    Update NganhService.java

--- src/main/java/sqa/example/service/NganhService.java
+++ src/main/java/sqa/example/service/NganhService.java
@@ -1,14 +1,21 @@
 package sqa.example.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sqa.example.model.Nganh;
 import sqa.example.repository.NganhRepository;
 
 @Service
 @RequiredArgsConstructor
 public class NganhService {
     private final NganhRepository nganhRepository;
-    public Integer getIdOfNganhByName(String name) {
-        return nganhRepository.getNganhByTen(name).getId();
+	
+    public List<Nganh> findAll() {
+        return nganhRepository.findAll();
+    }
+	
+    public Nganh findById(Integer id) {
+        return nganhRepository.findById(id).get();
     }
 }
