4a1f0a0adf8f1d17d3140815af352e2e62d9c3b8
huysvp <nvh.viethuy@gmail.com>
2023-04-19 18:36:04 +0700

    Update ThangDiemService.java

--- src/main/java/sqa/example/service/ThangDiemService.java
+++ src/main/java/sqa/example/service/ThangDiemService.java
@@ -12,38 +12,27 @@ public class ThangDiemService
 {
 	private final ThangDiemRepository thangDiemRepository;
 	
-	public List<ThangDiem> getAll() 
-	{
+	public List<ThangDiem> findAll() {
 		return thangDiemRepository.findAll();
 	}
 	
-	public ThangDiem get(Integer id) 
-	{
+	public ThangDiem findById(Integer id) {
         return thangDiemRepository.findById(id).get();
     }
 	
-	public ThangDiem getByName(String name) 
-	{
-        return thangDiemRepository.getThangDiemByName(name);
-    }
+	public ThangDiem findByName(String name) {
+		return thangDiemRepository.findByName(name);
+	}
 	
-	public ThangDiem create(ThangDiem entity) 
-	{		
-		if (thangDiemRepository.getThangDiemByName(entity.getDiemChu()) != null)
-			return null;
-			
+	public ThangDiem create(ThangDiem entity) {		
 		return thangDiemRepository.save(entity);
 	}
 	
-	public ThangDiem update(ThangDiem thangDiem) 
-	{
-		return thangDiemRepository.findById(thangDiem.getId())
-				.map(mh -> thangDiemRepository.save(thangDiem))
-				.orElseGet(() -> null);
+	public ThangDiem update(ThangDiem thangDiem) {
+		return thangDiemRepository.findById(thangDiem.getId()).map(mh -> thangDiemRepository.save(thangDiem)).orElseGet(() -> null);
     }
 	
-	public boolean delete(Integer id) 
-	{
+	public boolean delete(Integer id) {
 		var entity = thangDiemRepository.findById(id).get();
 		if (entity == null)
 			return false;
