a533ef1660aa97b2edb181541d7294836e36074b
huysvp <nvh.viethuy@gmail.com>
2023-04-25 23:02:26 +0700

    Delete src/main directory

--- src/main/resources/db/migration/V22__insert_data_to_tbl_ky_hoc.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO tbl_ky_hoc(id, ten) VALUES  (1, "Hoc Ky 1"),
-                                        (2, "Hoc Ky 2"),
-                                        (3, "Hoc Ky he"),
-                                        (4, "Hoc Ky dong");
--- src/main/resources/db/migration/V41__alter_to_lowercase.sql
+++
@@ -1,8 +0,0 @@
-UPDATE tbl_nguoi_dung SET gioi_tinh = LOWER(gioi_tinh), ten = LOWER(ten), dia_chi = LOWER(dia_chi), email = LOWER(email);
-UPDATE tbl_ky_hoc SET ten = LOWER(ten);
-UPDATE tbl_mon_hoc SET ten = LOWER(ten);
-UPDATE tbl_nganh SET ten = LOWER(ten);
-UPDATE tbl_nien_khoa SET ten = LOWER(ten);
-UPDATE tbl_phong_hoc SET ten = LOWER(ten);
-UPDATE tbl_sinh_vien SET ma_sinh_vien = LOWER(ma_sinh_vien)
-
